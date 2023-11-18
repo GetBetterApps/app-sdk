6866afb356bfb41bfb2e784e94dc6a2c6b9fc598
Artem Klimenko <klimenkocpp@icloud.com>
2023-11-18 15:09:49 +0600

    SHARED-4858

--- shared/features/follows/api/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/follows/api/FollowsRepository.kt
+++ shared/features/follows/api/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/follows/api/FollowsRepository.kt
@@ -1,3 +1,21 @@
 package com.velkonost.getbetter.shared.features.follows.api
 
-interface FollowsRepository
+import com.velkonost.getbetter.shared.core.util.ResultState
+import com.velkonost.getbetter.shared.features.follows.api.model.FollowsData
+import kotlinx.coroutines.flow.Flow
+
+interface FollowsRepository {
+
+    suspend fun addFollow(
+        followUserId: String
+    ): Flow<ResultState<FollowsData>>
+
+    suspend fun removeFollow(
+        followUserId: String
+    ): Flow<ResultState<FollowsData>>
+
+    suspend fun getUserFollowers(): Flow<ResultState<FollowsData>>
+
+    suspend fun getUserFollows(): Flow<ResultState<FollowsData>>
+
+}
--- shared/features/follows/data/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/follows/data/FollowsRepositoryImpl.kt
+++ shared/features/follows/data/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/follows/data/FollowsRepositoryImpl.kt
@@ -1,3 +1,12 @@
 package com.velkonost.getbetter.shared.features.follows.data
 
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.velkonost.getbetter.shared.features.follows.api.FollowsRepository
+import com.velkonost.getbetter.shared.features.follows.data.remote.FollowsRemoteDataSource
+
 class FollowsRepositoryImpl
+constructor(
+    private val remoteDataSource: FollowsRemoteDataSource,
+    private val localDataSource: DataStore<Preferences>
+) : FollowsRepository
