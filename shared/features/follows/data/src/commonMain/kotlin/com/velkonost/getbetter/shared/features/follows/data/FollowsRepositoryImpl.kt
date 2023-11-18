4c913a476a7a770dd5d5e4bdf548097053a606dc
Artem Klimenko <klimenkocpp@icloud.com>
2023-11-18 15:22:42 +0600

    SHARED-4858

--- shared/features/follows/data/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/follows/data/FollowsRepositoryImpl.kt
+++ shared/features/follows/data/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/follows/data/FollowsRepositoryImpl.kt
@@ -2,10 +2,15 @@ package com.velkonost.getbetter.shared.features.follows.data
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import com.velkonost.getbetter.shared.core.datastore.extension.getUserToken
 import com.velkonost.getbetter.shared.core.util.ResultState
+import com.velkonost.getbetter.shared.core.util.flowRequest
 import com.velkonost.getbetter.shared.features.follows.api.FollowsRepository
 import com.velkonost.getbetter.shared.features.follows.api.model.FollowsData
 import com.velkonost.getbetter.shared.features.follows.data.remote.FollowsRemoteDataSource
+import com.velkonost.getbetter.shared.features.follows.data.remote.model.request.UpdateFollowStateRequest
+import com.velkonost.getbetter.shared.features.follows.data.remote.model.response.UserType
+import com.velkonost.getbetter.shared.features.follows.data.remote.model.response.asExternalModel
 import kotlinx.coroutines.flow.Flow
 
 class FollowsRepositoryImpl
@@ -14,20 +19,40 @@ constructor(
     private val localDataSource: DataStore<Preferences>
 ) : FollowsRepository {
 
-    override suspend fun addFollow(followUserId: String): Flow<ResultState<FollowsData>> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun addFollow(followUserId: String): Flow<ResultState<FollowsData>> =
+        flowRequest(
+            mapper = { this asExternalModel UserType.Followers },
+            request = {
+                val token = localDataSource.getUserToken()
+                val body = UpdateFollowStateRequest(followUserId)
+                remoteDataSource.addFollow(token, body)
+            }
+        )
 
-    override suspend fun removeFollow(followUserId: String): Flow<ResultState<FollowsData>> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun removeFollow(followUserId: String): Flow<ResultState<FollowsData>> =
+        flowRequest(
+            mapper = { this asExternalModel UserType.Followers },
+            request = {
+                val token = localDataSource.getUserToken()
+                val body = UpdateFollowStateRequest(followUserId)
+                remoteDataSource.removeFollow(token, body)
+            }
+        )
 
-    override suspend fun getUserFollowers(): Flow<ResultState<FollowsData>> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getUserFollowers(): Flow<ResultState<FollowsData>> = flowRequest(
+        mapper = { this asExternalModel UserType.Followers },
+        request = {
+            val token = localDataSource.getUserToken()
+            remoteDataSource.fetchUserFollowers(token)
+        }
+    )
 
-    override suspend fun getUserFollows(): Flow<ResultState<FollowsData>> {
-        TODO("Not yet implemented")
-    }
+    override suspend fun getUserFollows(): Flow<ResultState<FollowsData>> = flowRequest(
+        mapper = { this asExternalModel UserType.Follows },
+        request = {
+            val token = localDataSource.getUserToken()
+            remoteDataSource.fetchUserFollows(token)
+        }
+    )
 
 }
--- shared/features/follows/data/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/follows/data/remote/model/response/KtorFollows.kt
+++ shared/features/follows/data/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/follows/data/remote/model/response/KtorFollows.kt
@@ -1,6 +1,8 @@
 package com.velkonost.getbetter.shared.features.follows.data.remote.model.response
 
+import com.velkonost.getbetter.shared.features.follows.api.model.FollowsData
 import com.velkonost.getbetter.shared.features.userinfo.data.remote.model.response.KtorUserInfoShort
+import com.velkonost.getbetter.shared.features.userinfo.data.remote.model.response.asExternalModel
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -9,3 +11,15 @@ data class KtorFollows(
     @SerialName("items")
     val items: List<KtorUserInfoShort>
 )
+
+infix fun KtorFollows.asExternalModel(userType: UserType): FollowsData {
+    return FollowsData(
+        followers = if (userType == UserType.Followers) this.items.map { it.asExternalModel() } else emptyList(),
+        follows = if (userType == UserType.Follows) this.items.map { it.asExternalModel() } else emptyList()
+    )
+}
+
+enum class UserType {
+    Followers,
+    Follows
+}
