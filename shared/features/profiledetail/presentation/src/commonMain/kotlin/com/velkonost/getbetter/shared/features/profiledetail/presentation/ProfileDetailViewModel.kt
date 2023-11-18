996323a5cdc5fb9be0ed6c16c1191698fb2d674e
Artem Klimenko <klimenkocpp@icloud.com>
2023-11-18 15:41:05 +0600

    SHARED-2228

--- shared/features/profiledetail/presentation/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/profiledetail/presentation/ProfileDetailViewModel.kt
+++ shared/features/profiledetail/presentation/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/profiledetail/presentation/ProfileDetailViewModel.kt
@@ -1,3 +1,24 @@
 package com.velkonost.getbetter.shared.features.profiledetail.presentation
 
+import com.velkonost.getbetter.shared.core.vm.BaseViewModel
+import com.velkonost.getbetter.shared.features.follows.api.FollowsRepository
+import com.velkonost.getbetter.shared.features.profiledetail.presentation.contract.ProfileDetailAction
+import com.velkonost.getbetter.shared.features.profiledetail.presentation.contract.ProfileDetailEvent
+import com.velkonost.getbetter.shared.features.profiledetail.presentation.contract.ProfileDetailNavigation
+import com.velkonost.getbetter.shared.features.profiledetail.presentation.contract.ProfileDetailViewState
+import com.velkonost.getbetter.shared.features.userinfo.api.UserInfoRepository
+
 class ProfileDetailViewModel
+internal constructor(
+    private val userInfoRepository: UserInfoRepository,
+    private val followsRepository: FollowsRepository
+) : BaseViewModel<ProfileDetailViewState, ProfileDetailAction, ProfileDetailNavigation, ProfileDetailEvent>(
+    initialState = ProfileDetailViewState()
+) {
+    override fun dispatch(action: ProfileDetailAction) = when (action) {
+        else -> {
+
+        }
+    }
+
+}
