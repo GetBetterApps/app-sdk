67685b9943487a56ea3528d8fc0d934e7dc5e185
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-11-21 20:15:44 +0600

    ANDROID-4205

--- shared/features/calendars/presentation/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/calendars/presentation/model/UserActionType.kt
+++ shared/features/calendars/presentation/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/calendars/presentation/model/UserActionType.kt
@@ -1,5 +1,7 @@
 package com.velkonost.getbetter.shared.features.calendars.presentation.model
 
+import com.velkonost.getbetter.shared.core.model.EntityType
+import com.velkonost.getbetter.shared.core.model.user.ActionType
 import com.velkonost.getbetter.shared.core.model.user.UserAction
 
 sealed interface UserActionType {
@@ -30,7 +32,26 @@ sealed interface UserActionType {
     data object UserGotFollower : UserActionType
 }
 
-val UserAction.type: UserActionType
-    get() {
+val UserAction.type: UserActionType?
+    get() = when {
+        entityType == EntityType.User && actionType == ActionType.Add -> UserActionType.UserRegistered
 
+        entityType == EntityType.Area && actionType == ActionType.Add -> UserActionType.UserCreatedArea
+        entityType == EntityType.Area && actionType == ActionType.Join -> UserActionType.UserJoinedArea
+        entityType == EntityType.Area && actionType == ActionType.Leave -> UserActionType.UserLeavedArea
+
+        entityType == EntityType.Note && actionType == ActionType.Add -> UserActionType.UserCreatedNote
+        entityType == EntityType.Note && actionType == ActionType.Complete -> UserActionType.UserCompletedGoal
+        entityType == EntityType.SubGoal && actionType == ActionType.Complete -> UserActionType.UserCompletedSubGoal
+
+        entityType == EntityType.Comment && actionType == ActionType.Add -> UserActionType.UserCreatedComment
+        entityType == EntityType.Comment && actionType == ActionType.AddInbox -> UserActionType.UserGotComment
+
+        entityType == EntityType.Like && actionType == ActionType.Add -> UserActionType.UserCreatedLike
+        entityType == EntityType.Like && actionType == ActionType.AddInbox -> UserActionType.UserGotLike
+
+        entityType == EntityType.Follow && actionType == ActionType.Add -> UserActionType.UserFollowed
+        entityType == EntityType.Follower && actionType == ActionType.Add -> UserActionType.UserGotFollower
+
+        else -> null
     }
