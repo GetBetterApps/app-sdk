a3474c62439661da971c881d0c0ee3f1e1382be6
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-11-24 16:29:33 +0600

    IOS-2151

--- iosApp/iosApp/App/AppDelegate.swift
+++ iosApp/iosApp/App/AppDelegate.swift
@@ -9,13 +9,28 @@
 import Foundation
 import SwiftUI
 import FirebaseCore
+import FirebaseMessaging
 
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+class AppDelegate: NSObject, UIApplicationDelegate, MessagingDelegate, UNUserNotificationCenterDelegate  {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        application.registerForRemoteNotifications()
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+    }
+    
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        if let fcm = Messaging.messaging().fcmToken {
+            print("fcm", fcm)
+        }
+    }
+    
 }
