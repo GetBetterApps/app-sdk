37ee04ae185dff77c15ca43caff9a0b0587cce07
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-11-25 16:47:08 +0600

    SHARED-4952

--- shared/features/feedback/api/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/feedback/api/FeedbackRepository.kt
+++ shared/features/feedback/api/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/feedback/api/FeedbackRepository.kt
@@ -1,3 +1,20 @@
 package com.velkonost.getbetter.shared.features.feedback.api
 
-interface FeedbackRepository
+import com.velkonost.getbetter.shared.core.model.feedback.Feedback
+import com.velkonost.getbetter.shared.core.model.feedback.FeedbackType
+import com.velkonost.getbetter.shared.core.util.ResultState
+import kotlinx.coroutines.flow.Flow
+
+interface FeedbackRepository {
+
+    fun createFeedback(
+        type: FeedbackType, text: String
+    ): Flow<ResultState<Feedback>>
+
+    fun addAnswer(
+        feedbackId: Int, text: String
+    ): Flow<ResultState<Feedback>>
+
+    fun getList(): Flow<ResultState<List<Feedback>>>
+
+}
