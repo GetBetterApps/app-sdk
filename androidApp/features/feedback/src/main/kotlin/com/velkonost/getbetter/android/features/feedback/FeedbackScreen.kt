306e8264faf5457c3cc7a9c0eed39a1a31490532
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-11-25 18:36:40 +0600

    SHARED-4952

--- androidApp/features/feedback/src/main/kotlin/com/velkonost/getbetter/android/features/feedback/FeedbackScreen.kt
+++ androidApp/features/feedback/src/main/kotlin/com/velkonost/getbetter/android/features/feedback/FeedbackScreen.kt
@@ -1,11 +1,17 @@
 package com.velkonost.getbetter.android.features.feedback
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.velkonost.getbetter.android.features.feedback.components.FeedbackItem
+import com.velkonost.getbetter.android.features.feedback.components.FeedbackListHeader
+import com.velkonost.getbetter.core.compose.components.Loader
 import com.velkonost.getbetter.shared.features.feedback.presentation.FeedbackViewModel
 
 @Composable
@@ -15,10 +21,27 @@ fun FeedbackScreen(
 ) {
     val state by viewModel.viewState.collectAsStateWithLifecycle()
 
-    LazyRow {
-        items(state.items, key = { it.id!! }) { item ->
+    Box(modifier = modifier.fillMaxSize()) {
+        if (state.isLoading && state.items.isEmpty()) {
+            Loader(modifier = Modifier.align(Alignment.Center))
+        } else {
+            LazyRow {
+                item {
+                    FeedbackListHeader {
 
+                    }
+                }
+
+                items(state.items, key = { it.id!! }) { item ->
+                    FeedbackItem(
+                        item = item,
+                        onClick = {
+
+                        }
+                    )
+                }
+            }
         }
-    }
 
+    }
 }
--- androidApp/features/feedback/src/main/kotlin/com/velkonost/getbetter/android/features/feedback/components/FeedbackItem.kt
+++ androidApp/features/feedback/src/main/kotlin/com/velkonost/getbetter/android/features/feedback/components/FeedbackItem.kt
@@ -1,11 +1,83 @@
 package com.velkonost.getbetter.android.features.feedback.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import com.velkonost.getbetter.core.compose.components.PrimaryBox
+import com.velkonost.getbetter.shared.core.model.feedback.Feedback
+import com.velkonost.getbetter.shared.core.model.feedback.FeedbackStatus
+import com.velkonost.getbetter.shared.resources.SharedR
+import dev.icerock.moko.resources.compose.colorResource
 
 @Composable
 fun FeedbackItem(
-    modifier: Modifier,
-    item: Feedback
+    modifier: Modifier = Modifier,
+    item: Feedback,
+    onClick: () -> Unit
 ) {
+
+    PrimaryBox {
+        Column(
+            modifier = modifier.clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null,
+                onClick = onClick
+            )
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    modifier = modifier
+                        .background(
+                            color = colorResource(resource = SharedR.colors.background_icon),
+                            shape = MaterialTheme.shapes.small
+                        )
+                        .padding(vertical = 4.dp, horizontal = 6.dp),
+                    text = item.type.responseName,
+                    style = MaterialTheme.typography.bodySmall,
+                    color = colorResource(resource = SharedR.colors.text_primary)
+                )
+                Spacer(modifier.weight(1f))
+
+                Box(
+                    modifier
+                        .size(12.dp)
+                        .background(
+                            color = colorResource(
+                                resource =
+                                if (item.status == FeedbackStatus.Opened) SharedR.colors.green
+                                else SharedR.colors.red
+                            ),
+                            shape = CircleShape
+                        )
+                )
+            }
+
+            Text(
+                modifier = modifier.padding(top = 12.dp),
+                text = item.messages.first().text,
+                style = MaterialTheme.typography.bodyMedium,
+                color = colorResource(resource = SharedR.colors.text_primary),
+                maxLines = 3,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+
 }
