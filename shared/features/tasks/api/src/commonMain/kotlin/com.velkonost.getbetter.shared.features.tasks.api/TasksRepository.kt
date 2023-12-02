a081cf2a535aa49654b13acec4e3c4cce41d9b0c
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-12-02 16:08:37 +0600

    SHARED-4195

--- shared/features/tasks/api/src/commonMain/kotlin/com.velkonost.getbetter.shared.features.tasks.api/TasksRepository.kt
+++ shared/features/tasks/api/src/commonMain/kotlin/com.velkonost.getbetter.shared.features.tasks.api/TasksRepository.kt
@@ -1,3 +1,39 @@
 package com.velkonost.getbetter.shared.features.tasks.api
 
-interface TasksRepository
+import com.velkonost.getbetter.shared.core.model.task.Task
+import com.velkonost.getbetter.shared.core.util.ResultState
+import kotlinx.coroutines.flow.Flow
+
+interface TasksRepository {
+
+    fun getCompletedTasks(): Flow<ResultState<List<Task>>>
+
+    fun getCurrentList(
+        forceUpdate: Boolean = false
+    ): Flow<ResultState<List<Task>>>
+
+    fun addToFavorite(
+        taskId: Int
+    ): Flow<ResultState<Task>>
+
+    fun removeFromFavorite(
+        taskId: Int
+    ): Flow<ResultState<Task>>
+
+    fun addToNotInteresting(
+        taskId: Int
+    ): Flow<ResultState<Task>>
+
+    fun removeFromNotInteresting(
+        taskId: Int
+    ): Flow<ResultState<Task>>
+
+    fun addToCompleted(
+        taskId: Int
+    ): Flow<ResultState<Task>>
+
+    fun removeFromCompleted(
+        taskId: Int
+    ): Flow<ResultState<Task>>
+
+}
