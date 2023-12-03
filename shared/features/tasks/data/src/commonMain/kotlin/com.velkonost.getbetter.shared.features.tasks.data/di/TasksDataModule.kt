0d7dd524d813668731b6a164995e6d69c217ac35
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-12-03 17:15:50 +0600

    SHARED-4195

--- shared/features/tasks/data/src/commonMain/kotlin/com.velkonost.getbetter.shared.features.tasks.data/di/TasksDataModule.kt
+++ shared/features/tasks/data/src/commonMain/kotlin/com.velkonost.getbetter.shared.features.tasks.data/di/TasksDataModule.kt
@@ -2,9 +2,13 @@ package com.velkonost.getbetter.shared.features.tasks.data.di
 
 import com.velkonost.getbetter.shared.features.tasks.api.TasksRepository
 import com.velkonost.getbetter.shared.features.tasks.data.TasksRepositoryImpl
+import com.velkonost.getbetter.shared.features.tasks.data.remote.TasksRemoteDataSource
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 
 val TasksDataModule = module {
+    singleOf(::TasksRemoteDataSource)
+
     single<TasksRepository> {
         TasksRepositoryImpl(get(), get())
     }
