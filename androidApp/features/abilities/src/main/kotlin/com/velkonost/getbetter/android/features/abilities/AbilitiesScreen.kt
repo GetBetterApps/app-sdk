2daf6c10f4f078193537f3108ddda2ba12d2b3d2
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-12-09 23:04:53 +0600

    SHARED-5124

--- androidApp/features/abilities/src/main/kotlin/com/velkonost/getbetter/android/features/abilities/AbilitiesScreen.kt
+++ androidApp/features/abilities/src/main/kotlin/com/velkonost/getbetter/android/features/abilities/AbilitiesScreen.kt
@@ -1,10 +1,20 @@
 package com.velkonost.getbetter.android.features.abilities
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.velkonost.getbetter.android.features.abilities.components.AbilityItem
+import com.velkonost.getbetter.core.compose.components.Loader
 import com.velkonost.getbetter.shared.features.abilities.presentation.AbilitiesViewModel
 
 @Composable
@@ -16,4 +26,26 @@ fun AbilitiesScreen(
     val listState = rememberLazyListState()
     val state by viewModel.viewState.collectAsStateWithLifecycle()
 
+    Box {
+        if (state.isLoading && state.items.isEmpty()) {
+            Loader(modifier = Modifier.align(Alignment.Center))
+        } else {
+            LazyColumn(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                contentPadding = PaddingValues(bottom = 140.dp, top = 28.dp)
+            ) {
+
+                items(state.items, key = { it.name }) { item ->
+                    AbilityItem(
+                        item = item,
+                        onClick = {
+
+                        }
+                    )
+                }
+            }
+        }
+    }
 }
---
+++ androidApp/features/abilities/src/main/kotlin/com/velkonost/getbetter/android/features/abilities/components/AbilityItem.kt
@@ -0,0 +1,83 @@
+package com.velkonost.getbetter.android.features.abilities.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.velkonost.getbetter.core.compose.components.PrimaryBox
+import com.velkonost.getbetter.shared.core.model.task.Ability
+import com.velkonost.getbetter.shared.resources.SharedR
+import dev.icerock.moko.resources.compose.colorResource
+import dev.icerock.moko.resources.compose.stringResource
+
+@Composable
+fun AbilityItem(
+    modifier: Modifier = Modifier,
+    item: Ability,
+    onClick: (Ability) -> Unit
+) {
+
+    val context = LocalContext.current
+
+    PrimaryBox {
+        Column(
+            modifier = modifier.clickable(
+                interactionSource = remember { MutableInteractionSource() },
+                indication = null
+            ) { onClick(item) }
+        ) {
+
+            Text(
+                text = item.name,
+                style = MaterialTheme.typography.labelLarge,
+                color = colorResource(resource = SharedR.colors.text_light)
+            )
+
+            Text(
+                modifier = modifier.padding(top = 12.dp),
+                text = item.description,
+                style = MaterialTheme.typography.labelMedium,
+                color = colorResource(resource = SharedR.colors.text_primary)
+            )
+
+            Row(
+                modifier = modifier.padding(top = 12.dp)
+            ) {
+                Text(
+                    text = stringResource(resource = SharedR.strings.experience_your_title),
+                    style = MaterialTheme.typography.labelMedium,
+                    color = colorResource(resource = SharedR.colors.text_primary)
+                )
+                Spacer(modifier.weight(1f))
+                Text(
+                    text = item.experienceData.currentLevelStr.toString(context),
+                    style = MaterialTheme.typography.labelMedium,
+                    color = colorResource(resource = SharedR.colors.text_primary)
+                )
+            }
+
+            LinearProgressIndicator(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
+                progress = item.experienceData.remainExperiencePercent,
+                color = colorResource(resource = SharedR.colors.button_gradient_start),
+                trackColor = colorResource(resource = SharedR.colors.text_unimportant_color),
+                strokeCap = StrokeCap.Round
+            )
+        }
+    }
+
+}
