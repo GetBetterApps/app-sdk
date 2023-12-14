76f48604e4bebf14d674f4cc85736084cb788337
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-12-14 14:57:08 +0600

    SHARED-5126

---
+++ iosApp/iosApp/Features/AbilityDetails/components/AbilityNotesContent.swift
@@ -0,0 +1,69 @@
+//
+//  AbilityNotesContent.swift
+//  iosApp
+//
+//  Created by velkonost on 14.12.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import SharedSDK
+
+struct AbilityNotesContent: View {
+    
+    private let isLoading: Bool
+    private let items: [Note]
+    private let loadMorePrefetch: Int
+    private let itemClick: (Note) -> Void
+    private let itemLikeClick: (Note) -> Void
+    private let onBottomReach: () -> Void
+    
+    init(isLoading: Bool, items: [Note], loadMorePrefetch: Int, itemClick: @escaping (Note) -> Void, itemLikeClick: @escaping (Note) -> Void, onBottomReach: @escaping () -> Void) {
+        self.isLoading = isLoading
+        self.items = items
+        self.loadMorePrefetch = loadMorePrefetch
+        self.itemClick = itemClick
+        self.itemLikeClick = itemLikeClick
+        self.onBottomReach = onBottomReach
+    }
+    
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            ScrollViewReader { value in
+                LazyVStack(spacing: 0) {
+                    ForEach(items, id: \.self.id) { item in
+                        NoteItem(
+                            item: item,
+                            onClick: itemClick,
+                            onLikeClick: itemLikeClick
+                        )
+                        .onAppear {
+                            checkPaginationThreshold(currentItemId: item.id)
+                        }
+                    }
+                    
+                    if isLoading {
+                        Loader().frame(alignment: .center)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            if items.isEmpty {
+                viewModel.dispatch(action: TaskDetailActionUserNotesLoadNextPage())
+            }
+        }
+    }
+}
+
+extension AbilityNotesContent {
+    func checkPaginationThreshold(currentItemId: Int32) {
+        let data = items
+        let thresholdIndex = data.index(data.endIndex, offsetBy: -loadMorePrefetch)
+        
+        if data.firstIndex(where: { $0.id == currentItemId })! >= thresholdIndex && !isLoading {
+            onBottomReach()
+        }
+    }
+}
