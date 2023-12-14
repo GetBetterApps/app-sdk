72c26853830bb995743c7c4c9e6cae56863d252a
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-12-14 20:17:10 +0600

    SHARED-5262

--- iosApp/iosApp/Features/Abilities/AbilitiesScreen.swift
+++ iosApp/iosApp/Features/Abilities/AbilitiesScreen.swift
@@ -25,7 +25,7 @@ struct AbilitiesScreen: View {
             } else {
                 ScrollView(showsIndicators: false) {
                     LazyVStack(spacing: 0) {
-                        ForEach(state.items, id: \.self.experienceData.id) { item in
+                        ForEach(state.items, id: \.self.id) { item in
                             AbilityItem(
                                 item: item,
                                 onClick: { value in
