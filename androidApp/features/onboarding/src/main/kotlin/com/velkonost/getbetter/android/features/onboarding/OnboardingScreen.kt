42788e6e660cfe4401813f0743cfcc2b2e2c3bb6
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-12-17 23:01:25 +0600

    SHARED-5328

--- androidApp/features/onboarding/src/main/kotlin/com/velkonost/getbetter/android/features/onboarding/OnboardingScreen.kt
+++ androidApp/features/onboarding/src/main/kotlin/com/velkonost/getbetter/android/features/onboarding/OnboardingScreen.kt
@@ -1,5 +1,7 @@
 package com.velkonost.getbetter.android.features.onboarding
 
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -16,6 +18,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -41,18 +44,26 @@ fun OnboardingScreen(
 
             Spacer(modifier.weight(1f))
 
-            Image(
-                modifier = modifier
-                    .shadow(
-                        elevation = 8.dp,
-                        shape = MaterialTheme.shapes.medium,
+            Box {
+                androidx.compose.animation.AnimatedVisibility(
+                    visible = state.step == 1,
+                    enter = fadeIn(),
+                    exit = fadeOut()
+                ) {
+                    Image(
+                        modifier = modifier
+                            .shadow(
+                                elevation = 8.dp,
+                                shape = MaterialTheme.shapes.medium,
+                            )
+                            .fillMaxHeight(0.6f)
+                            .clip(MaterialTheme.shapes.medium),
+                        painter = painterResource(imageResource = SharedR.images.ic_onboarding),
+                        contentDescription = null
                     )
-                    .fillMaxHeight(0.6f)
-                    .clip(MaterialTheme.shapes.medium)
-                    .align(Alignment.CenterHorizontally),
-                painter = painterResource(imageResource = SharedR.images.ic_onboarding),
-                contentDescription = null
-            )
+                }
+            }
+
 
             Spacer(modifier.weight(1f))
 
@@ -60,7 +71,7 @@ fun OnboardingScreen(
                 modifier = modifier
                     .padding(start = 32.dp, end = 32.dp)
                     .align(Alignment.CenterHorizontally),
-                text = stringResource(resource = SharedR.strings.onboarding_step_1),
+                text = state.title.toString(LocalContext.current),
                 style = MaterialTheme.typography.headlineLarge.copy(fontStyle = FontStyle.Italic),
                 color = colorResource(resource = SharedR.colors.text_title),
                 textAlign = TextAlign.Center
