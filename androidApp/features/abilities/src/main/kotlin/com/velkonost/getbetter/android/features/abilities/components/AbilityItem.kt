42723911ddf8ae7e3fc7817da0c91b2489aaf89a
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-12-17 13:54:52 +0600

    SHARED-5266

--- androidApp/features/abilities/src/main/kotlin/com/velkonost/getbetter/android/features/abilities/components/AbilityItem.kt
+++ androidApp/features/abilities/src/main/kotlin/com/velkonost/getbetter/android/features/abilities/components/AbilityItem.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import com.velkonost.getbetter.core.compose.components.PrimaryBox
 import com.velkonost.getbetter.shared.core.model.task.Ability
+import com.velkonost.getbetter.shared.features.abilities.presentation.model.FavoriteAbility
 import com.velkonost.getbetter.shared.resources.SharedR
 import dev.icerock.moko.resources.compose.colorResource
 import dev.icerock.moko.resources.compose.stringResource
@@ -30,6 +31,7 @@ fun AbilityItem(
 ) {
 
     val context = LocalContext.current
+    val isFavorite = item is FavoriteAbility
 
     PrimaryBox {
         Column(
@@ -40,43 +42,45 @@ fun AbilityItem(
         ) {
 
             Text(
-                text = item.name,
+                text = if (isFavorite) stringResource(resource = SharedR.strings.ability_favorite_title) else item.name,
                 style = MaterialTheme.typography.titleMedium,
                 color = colorResource(resource = SharedR.colors.text_light)
             )
 
             Text(
                 modifier = modifier.padding(top = 12.dp),
-                text = item.description,
+                text = if (isFavorite) stringResource(resource = SharedR.strings.ability_favorite_description) else item.description,
                 style = MaterialTheme.typography.labelMedium,
                 color = colorResource(resource = SharedR.colors.text_primary)
             )
 
-            Row(
-                modifier = modifier.padding(top = 12.dp)
-            ) {
-                Text(
-                    text = stringResource(resource = SharedR.strings.experience_your_title),
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = colorResource(resource = SharedR.colors.text_primary)
-                )
-                Spacer(modifier.weight(1f))
-                Text(
-                    text = item.experienceData.currentLevelStr.toString(context),
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = colorResource(resource = SharedR.colors.text_primary)
+            if (!isFavorite) {
+                Row(
+                    modifier = modifier.padding(top = 12.dp)
+                ) {
+                    Text(
+                        text = stringResource(resource = SharedR.strings.experience_your_title),
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = colorResource(resource = SharedR.colors.text_primary)
+                    )
+                    Spacer(modifier.weight(1f))
+                    Text(
+                        text = item.experienceData.currentLevelStr.toString(context),
+                        style = MaterialTheme.typography.bodyMedium,
+                        color = colorResource(resource = SharedR.colors.text_primary)
+                    )
+                }
+
+                LinearProgressIndicator(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    progress = item.experienceData.remainExperiencePercent,
+                    color = colorResource(resource = SharedR.colors.button_gradient_start),
+                    trackColor = colorResource(resource = SharedR.colors.text_unimportant_color),
+                    strokeCap = StrokeCap.Round
                 )
             }
-
-            LinearProgressIndicator(
-                modifier = modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp),
-                progress = item.experienceData.remainExperiencePercent,
-                color = colorResource(resource = SharedR.colors.button_gradient_start),
-                trackColor = colorResource(resource = SharedR.colors.text_unimportant_color),
-                strokeCap = StrokeCap.Round
-            )
         }
     }
 
