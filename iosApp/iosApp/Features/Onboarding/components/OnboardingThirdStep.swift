5f13985a1f5a42111dc02380dea698b10e3cd2ea
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-12-18 21:25:14 +0600

    SHARED-5332

---
+++ iosApp/iosApp/Features/Onboarding/components/OnboardingThirdStep.swift
@@ -0,0 +1,42 @@
+//
+//  OnboardingThirdStep.swift
+//  iosApp
+//
+//  Created by velkonost on 18.12.2023.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+import SharedSDK
+import SwiftUIPager
+
+struct OnboardingThirdStep: View {
+    
+    @Binding var textVisible: Bool
+    let items: [Ability]
+    
+    init(textVisible: Binding<Bool>, items: [Ability]) {
+        self._textVisible = textVisible
+        self.items = items
+    }
+    
+    @StateObject var page: Page = .first()
+    
+    var body: some View {
+        Pager(
+            page: page,
+            data: items,
+            id: \.self.id
+        ) { areaWrapper in
+//            AreaPickerItem(area: areaWrapper.area)
+        }
+        .interactive(rotation: true)
+        .interactive(scale: 0.8)
+        .alignment(.center)
+        .preferredItemSize(CGSize(width: 300, height: 150))
+        .frame(height: 150)
+        .padding(.bottom, 16)
+        
+    }
+}
