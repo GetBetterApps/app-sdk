bbb98ac64d2c8fa4ad40ad4600ca57614dbfdbd7
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-12-18 16:14:32 +0600

    SHARED-5328

--- iosApp/iosApp/Features/Onboarding/components/OnboardingSecondStep.swift
+++ iosApp/iosApp/Features/Onboarding/components/OnboardingSecondStep.swift
@@ -13,25 +13,60 @@ import SharedSDK
 struct OnboardingSecondStep: View {
     
     @State var imageIndex: Int = 0
+    private let durationPerImage: Double = 3.5
     
     
     var body: some View {
         @State var rotation: Angle = Angle(degrees: Double(imageIndex) * Double(180))
         
-        Image(uiImage: SharedR.images().ic_onboarding_2_1.toUIImage()!)
-            .resizable()
-            .shadow(radius: 8)
-            .scaledToFit()
-            .frame(maxWidth: .infinity)
-            .padding(.horizontal, 32)
+        @State var imageRotationDegrees: Double = if rotation.degrees <= 90 {
+            0
+        } else if rotation.degrees <= 180 {
+            180
+        } else if rotation.degrees <= 360 {
+            0
+        } else if rotation.degrees <= 540 {
+            180
+        } else {
+            0
+        }
+        
+        @State var imageRotation: Angle = Angle(degrees: imageRotationDegrees)
+        
+        ZStack {
+            Image(uiImage: SharedR.images().ic_onboarding_2_1.toUIImage()!)
+                .resizable()
+                .shadow(radius: 8)
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal, 32)
+                .rotation3DEffect(
+                    imageRotation,
+                    axis: (x: 0.0, y: 1.0, z: 0.0)
+                )
+        }
+       
             .rotation3DEffect(
                 rotation,
-                axis: (x: 1.0, y: 0.0, z: 0.0)
+                axis: (x: 0.0, y: 1.0, z: 0.0)
             )
+//            .animation(.easeInOut(duration: 1).delay(durationPerImage), value: rotation)
             .onAppear {
-                withAnimation(.easeInOut(duration: 0.5).delay(3.5)) {
+//                withAnimation(.easeInOut(duration: 0.5).delay(durationPerImage)) {
                     imageIndex += 1
-                }
+//                }
+                
+//                withAnimation(.easeInOut(duration: 0.5).delay(2 * durationPerImage)) {
+//                    imageIndex += 1
+//                }
+                
+//                withAnimation(.easeInOut(duration: 0.5).delay(3 * durationPerImage)) {
+//                    imageIndex += 1
+//                }
+                
+//                withAnimation(.easeInOut(duration: 0.5).delay(4 * durationPerImage)) {
+//                    imageIndex += 1
+//                }
             }
     }
 }
