49331eae185ffb4e4902c0c3dbaad6eb9691bd71
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2023-12-18 15:05:37 +0600

    SHARED-5328

--- androidApp/features/onboarding/src/main/kotlin/com/velkonost/getbetter/android/features/onboarding/components/OnboardingSecondStep.kt
+++ androidApp/features/onboarding/src/main/kotlin/com/velkonost/getbetter/android/features/onboarding/components/OnboardingSecondStep.kt
@@ -8,18 +8,26 @@ import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
 import com.velkonost.getbetter.shared.resources.SharedR
 import dev.icerock.moko.resources.compose.painterResource
+import kotlinx.coroutines.delay
 
 @OptIn(ExperimentalMaterialApi::class)
 @Composable
@@ -29,15 +37,31 @@ fun BoxScope.OnboardingSecondStep(
 ) {
 
     val animationDuration = 1200
+    val durationPerImage = 3500L
+    val imageIndex = remember { mutableIntStateOf(0) }
 
     val rotation by animateFloatAsState(
-        targetValue = 180f,
+        targetValue = imageIndex.intValue * 180f,
         animationSpec = tween(
-            durationMillis = 400,
+            durationMillis = 800,
             easing = FastOutSlowInEasing,
         ), label = ""
     )
-    val imageIndex = 1
+
+    LaunchedEffect(Unit) {
+        delay(durationPerImage)
+        imageIndex.intValue++
+
+        delay(durationPerImage)
+        imageIndex.intValue++
+
+        delay(durationPerImage)
+        imageIndex.intValue++
+
+        delay(durationPerImage)
+        imageIndex.intValue++
+    }
+
 
     Column(
         modifier
@@ -63,16 +87,43 @@ fun BoxScope.OnboardingSecondStep(
             )
         ) {
 
-            Card(
-//                onClick = { onClick(cardFace) },
+            Box(
+
                 modifier = modifier
+                    .padding(horizontal = 32.dp)
                     .graphicsLayer {
                         rotationY = rotation
                         cameraDistance = 12f * density
                     },
             ) {
+
                 Image(
-                    painter = painterResource(imageResource = SharedR.images.ic_onboarding_1_2),
+                    modifier = modifier
+                        .shadow(
+                            elevation = 8.dp,
+                            shape = MaterialTheme.shapes.medium,
+                        )
+                        .graphicsLayer {
+                            rotationY =
+                                when {
+                                    rotation <= 90f -> 0f
+                                    rotation <= 180f -> 180f
+                                    rotation <= 360f -> 0f
+                                    rotation <= 540f -> 180f
+                                    else -> 0f
+                                }
+
+                        },
+                    painter = painterResource(
+                        imageResource =
+                        when {
+                            rotation <= 90f -> SharedR.images.ic_onboarding_2_1
+                            rotation <= 180f -> SharedR.images.ic_onboarding_2_2
+                            rotation <= 360f -> SharedR.images.ic_onboarding_2_3
+                            rotation <= 540f -> SharedR.images.ic_onboarding_2_4
+                            else -> SharedR.images.ic_onboarding_2_5
+                        }
+                    ),
                     contentDescription = null
                 )
             }
