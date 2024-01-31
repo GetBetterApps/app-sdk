4277b77ef1190e5e3bec2160861c7fa6f1825826
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2024-02-01 01:34:25 +0600

    5472

---
+++ shared/core/util/src/commonMain/kotlin/com.velkonost.getbetter.shared.core.util/advertise/AdId.kt
@@ -0,0 +1,5 @@
+package com.velkonost.getbetter.shared.core.util.advertise
+
+import com.velkonost.getbetter.UtilBuildKonfig
+
+var ACTUAL_AD_ID = UtilBuildKonfig.AD_ID//UtilBuildKonfig.RUSTORE_AD_ID
--- shared/features/social/presentation/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/social/contracts/SocialViewState.kt
+++ shared/features/social/presentation/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/social/contracts/SocialViewState.kt
@@ -1,8 +1,8 @@
 package com.velkonost.getbetter.shared.features.social.contracts
 
-import com.velkonost.getbetter.UtilBuildKonfig
 import com.velkonost.getbetter.shared.core.model.note.Note
 import com.velkonost.getbetter.shared.core.util.PrefetchDistanceValue
+import com.velkonost.getbetter.shared.core.util.advertise.ACTUAL_AD_ID
 import com.velkonost.getbetter.shared.core.vm.contracts.UIContract
 import com.velkonost.getbetter.shared.features.social.model.SocialTab
 
@@ -11,7 +11,7 @@ data class SocialViewState(
     val generalFeed: FeedViewState = FeedViewState(),
     val areasFeed: FeedViewState = FeedViewState(),
     val adPosition: Int = (6..10).random(),
-    val adId: String = UtilBuildKonfig.AD_ID//UtilBuildKonfig.RUSTORE_AD_ID
+    val adId: String = ACTUAL_AD_ID
 ) : UIContract.State
 
 data class FeedViewState(
