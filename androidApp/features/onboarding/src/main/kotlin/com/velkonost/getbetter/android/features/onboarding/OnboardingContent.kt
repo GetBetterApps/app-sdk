0a732e7e53ee630a426c7d1e58371435f64c9787
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2024-01-31 18:09:57 +0600

    5705

--- androidApp/features/onboarding/src/main/kotlin/com/velkonost/getbetter/android/features/onboarding/OnboardingContent.kt
+++ androidApp/features/onboarding/src/main/kotlin/com/velkonost/getbetter/android/features/onboarding/OnboardingContent.kt
@@ -1,6 +1,6 @@
 package com.velkonost.getbetter.android.features.onboarding
 
-import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
@@ -67,6 +67,8 @@ fun OnboardingContent(
             forthPointVisible.value = true
             delay(2000)
             fifthPointVisible.value = true
+            delay(500)
+            buttonVisible.value = true
         }
     }
 
@@ -103,48 +105,61 @@ fun OnboardingContent(
 
         Spacer(modifier.height(32.dp))
 
-        AnimatedContent(targetState = firstPointVisible, label = "") {
-            if (it.value) {
-                OnboardingPoint(title = stringResource(resource = SharedR.strings.onboarding_step_1))
-            }
+        AnimatedVisibility(
+            visible = firstPointVisible.value, label = ""
+        ) {
+            OnboardingPoint(
+                title = stringResource(resource = SharedR.strings.onboarding_step_1),
+                visible = firstPointVisible.value
+            )
         }
 
-        AnimatedContent(targetState = secondPointVisible, label = "") {
-            if (it.value) {
-                OnboardingPoint(title = stringResource(resource = SharedR.strings.onboarding_step_2))
-            }
+        AnimatedVisibility(
+            visible = secondPointVisible.value, label = ""
+        ) {
+            OnboardingPoint(
+                title = stringResource(resource = SharedR.strings.onboarding_step_2),
+                visible = secondPointVisible.value
+            )
         }
 
-        AnimatedContent(targetState = thirdPointVisible, label = "") {
-            if (it.value) {
-                OnboardingPoint(title = stringResource(resource = SharedR.strings.onboarding_step_3))
-            }
+        AnimatedVisibility(
+            visible = thirdPointVisible.value, label = ""
+        ) {
+            OnboardingPoint(
+                title = stringResource(resource = SharedR.strings.onboarding_step_3),
+                visible = thirdPointVisible.value
+            )
         }
 
-        AnimatedContent(targetState = forthPointVisible, label = "") {
-            if (it.value) {
-                OnboardingPoint(title = stringResource(resource = SharedR.strings.onboarding_step_4))
-            }
+        AnimatedVisibility(
+            visible = forthPointVisible.value, label = ""
+        ) {
+            OnboardingPoint(
+                title = stringResource(resource = SharedR.strings.onboarding_step_4),
+                visible = forthPointVisible.value
+            )
         }
 
-        AnimatedContent(targetState = fifthPointVisible, label = "") {
-            if (it.value) {
-                OnboardingPoint(title = stringResource(resource = SharedR.strings.onboarding_step_5))
-            }
+        AnimatedVisibility(
+            visible = fifthPointVisible.value, label = ""
+        ) {
+            OnboardingPoint(
+                title = stringResource(resource = SharedR.strings.onboarding_step_5),
+                visible = fifthPointVisible.value
+            )
         }
 
         Spacer(modifier.weight(1f))
 
-        AnimatedContent(targetState = buttonVisible, label = "") {
-            if (it.value) {
-                AppButton(
-                    modifier = modifier
-                        .align(Alignment.CenterHorizontally),
-                    labelText = stringResource(resource = SharedR.strings.onboarding_btn),
-                    isLoading = isLoading,
-                    onClick = onNextClick
-                )
-            }
+        AnimatedVisibility(visible = buttonVisible.value, label = "") {
+            AppButton(
+                modifier = modifier
+                    .align(Alignment.CenterHorizontally),
+                labelText = stringResource(resource = SharedR.strings.onboarding_btn),
+                isLoading = isLoading,
+                onClick = onNextClick
+            )
         }
 
         Spacer(modifier = modifier.height(64.dp))
@@ -155,7 +170,8 @@ fun OnboardingContent(
 @Composable
 fun OnboardingPoint(
     modifier: Modifier = Modifier,
-    title: String
+    title: String,
+    visible: Boolean
 ) {
     val composition by rememberLottieComposition(
         LottieCompositionSpec.RawRes(SharedR.files.anim_mark.rawResId)
@@ -164,34 +180,39 @@ fun OnboardingPoint(
     val scope = rememberCoroutineScope()
     val animVisible = remember { mutableStateOf(false) }
 
-    LaunchedEffect(Unit) {
-        scope.launch {
-            delay(1500)
-            animVisible.value = true
+    LaunchedEffect(visible) {
+        if (visible) {
+            scope.launch {
+                delay(500)
+                animVisible.value = true
+            }
         }
     }
 
     Row(
         modifier = modifier
-            .padding(top = 16.dp, start = 16.dp, end = 16.dp)
+            .padding(top = 8.dp, start = 16.dp, end = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
         if (animVisible.value) {
             LottieAnimation(
-                modifier = modifier.size(24.dp),
+                modifier = modifier.size(48.dp),
                 composition = composition,
                 iterations = 1,
             )
         } else {
-            Spacer(modifier.size(24.dp))
+            Spacer(modifier.size(48.dp))
         }
 
         Text(
-            modifier = modifier.padding(start = 8.dp),
+//            modifier = modifier.padding(start = 8.dp),
             text = title.uppercase(),
             style = MaterialTheme.typography.labelMedium,
             color = colorResource(resource = SharedR.colors.text_title),
             textAlign = TextAlign.Start,
         )
+
+        Spacer(modifier.weight(1f))
     }
 
 }
