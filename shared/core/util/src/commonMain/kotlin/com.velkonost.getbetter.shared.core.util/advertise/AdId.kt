044e88d3fecaf3c1a38c22c8c2a3763408eaf597
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2024-02-01 14:18:03 +0600

    v.1.2

--- shared/core/util/src/commonMain/kotlin/com.velkonost.getbetter.shared.core.util/advertise/AdId.kt
+++ shared/core/util/src/commonMain/kotlin/com.velkonost.getbetter.shared.core.util/advertise/AdId.kt
@@ -2,4 +2,4 @@ package com.velkonost.getbetter.shared.core.util.advertise
 
 import com.velkonost.getbetter.UtilBuildKonfig
 
-var ACTUAL_AD_ID = UtilBuildKonfig.AD_ID//UtilBuildKonfig.RUSTORE_AD_ID
+var ACTUAL_AD_ID: String = UtilBuildKonfig.AD_ID//UtilBuildKonfig.RUSTORE_AD_ID.toString()
