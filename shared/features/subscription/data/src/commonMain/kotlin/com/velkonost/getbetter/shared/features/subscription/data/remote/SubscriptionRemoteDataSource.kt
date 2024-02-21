9d5adf34d73b1549e40a626f520f992fa4e697c3
Artem Klimenko <17786225+Velkonost@users.noreply.github.com>
2024-02-21 22:53:33 +0600

    1941

--- shared/features/subscription/data/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/subscription/data/remote/SubscriptionRemoteDataSource.kt
+++ shared/features/subscription/data/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/subscription/data/remote/SubscriptionRemoteDataSource.kt
@@ -1,7 +1,14 @@
 package com.velkonost.getbetter.shared.features.subscription.data.remote
 
+import com.velkonost.getbetter.shared.core.network.extensions.makeRequest
 import com.velkonost.getbetter.shared.core.network.model.RemoteResponse
+import com.velkonost.getbetter.shared.features.subscription.data.remote.model.request.CreateSubscriptionRequest
+import com.velkonost.getbetter.shared.features.subscription.data.remote.model.response.KtorCreateSubscription
+import com.velkonost.getbetter.shared.features.subscription.data.remote.model.response.KtorSubscription
 import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.request.get
+import io.ktor.client.request.post
 
 class SubscriptionRemoteDataSource(
     private val httpClient: HttpClient
@@ -9,6 +16,49 @@ class SubscriptionRemoteDataSource(
 
     suspend fun startTrial(
         token: String?
-    ): RemoteResponse<>
+    ): RemoteResponse<KtorSubscription> = httpClient.post {
+        makeRequest(
+            path = Route.START_TRIAL,
+            token = token
+        )
+    }.body()
+
+    suspend fun createSubscription(
+        token: String?,
+        body: CreateSubscriptionRequest
+    ): RemoteResponse<KtorCreateSubscription> = httpClient.post {
+        makeRequest(
+            path = Route.CREATE,
+            token = token,
+            body = body
+        )
+    }.body()
+
+    suspend fun cancelAutoRenewal(
+        token: String?
+    ): RemoteResponse<KtorSubscription> = httpClient.post {
+        makeRequest(
+            path = Route.CANCEL,
+            token = token
+        )
+    }.body()
+
+    suspend fun isSubscriptionsAvailable(
+        token: String?
+    ): RemoteResponse<Boolean> = httpClient.get {
+        makeRequest(
+            path = Route.AVAILABLE,
+            token = token
+        )
+    }.body()
+
+    suspend fun getSubscriptionStatus(
+        token: String?
+    ): RemoteResponse<KtorSubscription> = httpClient.get {
+        makeRequest(
+            path = Route.STATUS,
+            token = token
+        )
+    }.body()
 
 }
--- shared/features/subscription/presentation/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/subscription/presentation/model/SubscriptionType.kt
+++ shared/features/subscription/presentation/src/commonMain/kotlin/com/velkonost/getbetter/shared/features/subscription/presentation/model/SubscriptionType.kt
@@ -7,23 +7,27 @@ import dev.icerock.moko.resources.desc.StringDesc
 enum class SubscriptionType(
     val title: StringDesc,
     val text: StringDesc,
-    val price: StringDesc
+    val price: StringDesc,
+    val responseName: String
 ) {
     Month(
         title = StringDesc.Resource(SharedR.strings.offer_month_title),
         text = StringDesc.Resource(SharedR.strings.offer_month_text),
-        price = StringDesc.Resource(SharedR.strings.offer_month_price)
+        price = StringDesc.Resource(SharedR.strings.offer_month_price),
+        responseName = "month"
     ),
 
     Year(
         title = StringDesc.Resource(SharedR.strings.offer_year_title),
         text = StringDesc.Resource(SharedR.strings.offer_year_text),
-        price = StringDesc.Resource(SharedR.strings.offer_year_price)
+        price = StringDesc.Resource(SharedR.strings.offer_year_price),
+        responseName = "year"
     ),
 
     Unlimited(
         title = StringDesc.Resource(SharedR.strings.offer_unlimited_title),
         text = StringDesc.Resource(SharedR.strings.offer_unlimited_text),
-        price = StringDesc.Resource(SharedR.strings.offer_unlimited_price)
+        price = StringDesc.Resource(SharedR.strings.offer_unlimited_price),
+        responseName = "unlimited"
     )
 }
